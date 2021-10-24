19a26bd5425886e13a21a8cf8b865a76322801bb
Filip <szymanski.fp@gmail.com>
2021-10-24 11:03:34 +0200

    Initialized Backend in Java Spring with web, sql jpa and sql driver dependencies

---
+++ Backend/src/main/java/eu/filip/getemployeesapp/GetEmployeesAppApplication.java
@@ -0,0 +1,13 @@
+package eu.filip.getemployeesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetEmployeesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetEmployeesAppApplication.class, args);
+    }
+
+}
