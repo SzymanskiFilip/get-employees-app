7249ed425bf988ab8e735d871deccc3f7b9b9535
Filip <szymanski.fp@gmail.com>
2021-10-26 22:31:32 +0200

    Created Employee Entity

---
+++ Backend/src/main/java/eu/filip/getemployeesapp/entities/Employee.java
@@ -0,0 +1,88 @@
+package eu.filip.getemployeesapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Employee {
+    private @Id Long id;
+    private String first_name;
+    private String last_name;
+    private int age;
+    private String profession;
+    private boolean status;
+
+    public Employee() {}
+
+    public Employee(Long id, String first_name, String last_name, int age, String profession, boolean status) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.profession = profession;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return age == employee.age && status == employee.status && id.equals(employee.id) && Objects.equals(first_name, employee.first_name) && Objects.equals(last_name, employee.last_name) && Objects.equals(profession, employee.profession);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, first_name, last_name, age, profession, status);
+    }
+}
