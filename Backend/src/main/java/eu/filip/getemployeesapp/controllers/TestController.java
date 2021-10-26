91087d0c234028afe2d8f931bf2649a60b4447ba
Filip <szymanski.fp@gmail.com>
2021-10-26 22:57:31 +0200

    Added a test endpoint to the TestController

--- Backend/src/main/java/eu/filip/getemployeesapp/controllers/TestController.java
+++ Backend/src/main/java/eu/filip/getemployeesapp/controllers/TestController.java
@@ -1,15 +1,29 @@
 package eu.filip.getemployeesapp.controllers;
 
+import eu.filip.getemployeesapp.entities.Employee;
+import eu.filip.getemployeesapp.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TestController {
 
+    private final EmployeeRepository repository;
+    public TestController(EmployeeRepository repo){
+        this.repository = repo;
+    }
+
     @GetMapping("/test")
-    public String test(){
+    String test(){
         return "Hello API!";
+    }
+
+    @GetMapping("/repoTest")
+    List<Employee> repoTest(){
+        return repository.findAll();
     }
 }
