8a8fab7a7cf781d6a7a95171a4fdecf640e48abc
Filip <szymanski.fp@gmail.com>
2021-10-27 21:30:13 +0200

    added Filter component to app

---
+++ Frontend/get-employees-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Filter from './Components/Filter.js';
+
+const App = () =>{
+    return(
+        <div>
+            <Filter/>
+        </div>
+    )
+}
+
+export default App;
