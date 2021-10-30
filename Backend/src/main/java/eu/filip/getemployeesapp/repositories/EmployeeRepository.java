0b3c49d5ea5a92b7b02ef4e5a6c6db6f4c45dbb0
Filip <szymanski.fp@gmail.com>
2021-10-30 22:19:27 +0200

    added a custom repo function that returns employees by profession

--- Backend/src/main/java/eu/filip/getemployeesapp/repositories/EmployeeRepository.java
+++ Backend/src/main/java/eu/filip/getemployeesapp/repositories/EmployeeRepository.java
@@ -2,6 +2,13 @@ package eu.filip.getemployeesapp.repositories;
 
 import eu.filip.getemployeesapp.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "SELECT * FROM employees WHERE profession = profession", nativeQuery = true)
+    List<Employee> findByProfession(String profession);
+
 }
