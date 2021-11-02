c0d761a31fe33e0d304e83c10dd49d554bad6927
Filip <szymanski.fp@gmail.com>
2021-11-02 17:23:24 +0100

    conditional class of hired or not hired

--- Frontend/get-employees-app/src/Components/Employee.js
+++ Frontend/get-employees-app/src/Components/Employee.js
@@ -1,9 +1,11 @@
 const Employee = (props) => {
     return(
-      <div>
+      <div className="employee">
           <h1>{props.firstName} {props.lastName}, {props.age}</h1>
           <h3>{props.profession}</h3>
-          <h3>
+          <h3 className={
+              props.status ? 'hired' : 'not-hired'
+          }>
               {props.status ? 'Already Hired' : 'looking for a job'}
           </h3>
       </div>
