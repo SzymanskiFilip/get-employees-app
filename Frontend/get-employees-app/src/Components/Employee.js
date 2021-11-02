db025aa13a346f227f8d75a2118ff4b1193b5463
Filip <szymanski.fp@gmail.com>
2021-11-02 17:11:52 +0100

    changed tags

--- Frontend/get-employees-app/src/Components/Employee.js
+++ Frontend/get-employees-app/src/Components/Employee.js
@@ -1,8 +1,8 @@
 const Employee = (props) => {
     return(
       <div>
-          <h3>{props.firstName} {props.lastName}, {props.age}</h3>
-          <h1>{props.profession}</h1>
+          <h1>{props.firstName} {props.lastName}, {props.age}</h1>
+          <h3>{props.profession}</h3>
           <h3>
               {props.status ? 'Already Hired' : 'looking for a job'}
           </h3>
