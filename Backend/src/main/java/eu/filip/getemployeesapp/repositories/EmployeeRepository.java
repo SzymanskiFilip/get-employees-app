a045b1f6d7ba5cff18e710de1262dff0eea8d539
Filip <szymanski.fp@gmail.com>
2021-11-02 20:11:20 +0100

    post mapping employee saves employee based on passed json

--- Backend/src/main/java/eu/filip/getemployeesapp/repositories/EmployeeRepository.java
+++ Backend/src/main/java/eu/filip/getemployeesapp/repositories/EmployeeRepository.java
@@ -17,4 +17,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query(value = "SELECT * FROM employees WHERE profession = ?1 AND status = ?2", nativeQuery = true)
     List<Employee> findByProfessionAndStatus(String profession, boolean status);
 
+    @Query(value= "INSERT INTO employees (first_name, last_name, age, profession, status) VALUES (?1, ?2, ?3, ?4, ?5)", nativeQuery = true)
+    Employee saveEmployee(String first_name, String last_name, int age, String profession, boolean status);
 }
