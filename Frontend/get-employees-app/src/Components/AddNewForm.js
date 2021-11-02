3118f4dde4ce7ab44c287b70c9e92bbb2b7d5ef1
Filip <szymanski.fp@gmail.com>
2021-11-02 19:44:28 +0100

    finished form component, create post request

--- Frontend/get-employees-app/src/Components/AddNewForm.js
+++ Frontend/get-employees-app/src/Components/AddNewForm.js
@@ -1,6 +1,49 @@
+import {useState} from 'react';
+
 const AddNewForm = () => {
+
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [age, setAge] = useState(0);
+    const [profession, setProfession] = useState("");
+    const [employmentStatus, setEmploymentStatus] = useState(false);
+
+    const handleSubmit = () =>{
+        const newEmployee = {
+            first_name: name,
+            last_name: lastName,
+            age: parseInt(age),
+            profession: profession,
+            status: employmentStatus
+        }
+        fetch("api/v1/employee", {
+            
+        });
+    }
+
     return(
-        1
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+            }
+            }>
+            <label>Name:</label>
+            <input required type="text" name="name"
+                   onChange={(e) => setName(e.target.value)}/>
+            <label>Last Name:</label>
+            <input required type="text" name="last_name"
+                   onChange={(e) => setLastName(e.target.value)}/>
+            <label>Age:</label>
+            <input required type="number" name="age"
+                    onChange={(e) => setAge(e.target.value)}/>
+            <label>Profession:</label>
+            <input required type="text" name="profession"
+                   onChange={(e) => setProfession(e.target.value)}/>
+            <label>Employment Status:</label>
+            <input required type="checkbox" id="bigger-checkbox" name="employment_status"
+                   onChange={(e) => setEmploymentStatus(e.target.checked)}/>
+            <button type="submit">Submit</button>
+        </form>
     );
 }
 
