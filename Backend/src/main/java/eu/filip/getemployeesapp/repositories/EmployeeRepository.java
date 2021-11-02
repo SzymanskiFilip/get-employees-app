5ad2048bc9ac63eae4237206c7bab099cbae2b24
Filip <szymanski.fp@gmail.com>
2021-11-02 20:41:12 +0100

    changed JPA repo to Crud repo'

--- Backend/src/main/java/eu/filip/getemployeesapp/repositories/EmployeeRepository.java
+++ Backend/src/main/java/eu/filip/getemployeesapp/repositories/EmployeeRepository.java
@@ -3,10 +3,13 @@ package eu.filip.getemployeesapp.repositories;
 import eu.filip.getemployeesapp.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+    List<Employee> findAll();
 
     @Query(value = "SELECT * FROM employees WHERE profession = ?1", nativeQuery = true)
     List<Employee> findByProfession(String profession);
@@ -17,6 +20,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query(value = "SELECT * FROM employees WHERE profession = ?1 AND status = ?2", nativeQuery = true)
     List<Employee> findByProfessionAndStatus(String profession, boolean status);
 
-    @Query(value= "INSERT INTO employees (first_name, last_name, age, profession, status) VALUES (?1, ?2, ?3, ?4, ?5)", nativeQuery = true)
-    Employee saveEmployee(String first_name, String last_name, int age, String profession, boolean status); 
+    //@Query(value= "INSERT INTO employees (first_name, last_name, age, profession, status) VALUES (?1, ?2, ?3, ?4, ?5)", nativeQuery = true)
+    //Employee saveEmployee(String first_name, String last_name, int age, String profession, boolean status);
 }
